143efb45d802e8602ee61d80034cdcd0c12134d8
Jeff Linn <linnterweb@gmail.com>
2017-10-20 11:42:18 -0400

    Integrated most changes for landing page

---
+++ js/typer.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  var line1 = {
+    strings: ["Around Town"],
+    typeSpeed: 40
+  };
+  var line2 = {
+    strings: ["Across the State"],
+    typeSpeed: 40
+  };
+  var line3 = {
+    strings: ["Nationwide"],
+    typeSpeed: 40
+  };
+  // var options = {
+  //     strings: ["Around Town", "Across the State", "Nationwide"],
+  //     typeSpeed: 40,
+  //   }
+  function typer() {
+    setTimeout(function() {
+      var typed1 = new Typed("#typing1", line1);
+    }, 300);
+    setTimeout(function() {
+      var typed2 = new Typed("#typing2", line2);
+    }, 1300);
+    setTimeout(function() {
+      var typed3 = new Typed("#typing3", line3);
+    }, 2600);
+  }
+  typer();
+});

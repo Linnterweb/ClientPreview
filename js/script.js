b0d00f81c8dc19c72ec0b5a272f37742af6d65d5
Jeff Linn <linnterweb@gmail.com>
2017-11-14 00:08:20 -0600

    Modal and slider fix

--- js/script.js
+++ js/script.js
@@ -2,45 +2,54 @@ $(document).ready(function() {
   new WOW().init();
 
   // Add smooth scrolling to all links
-  $("a").on("click", function(event) {
+  // $("a").on("click", function(event) {
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    // if (this.hash !== "") {
       // Prevent default anchor click behavior
-      event.preventDefault();
+      // event.preventDefault();
 
       // Store hash
-      var hash = this.hash;
+      // var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate(
-        {
-          scrollTop: $(hash).offset().top - 110
-        },
-        800,
-        function() {
+      // $("html, body").animate(
+        // {
+          // scrollTop: $(hash).offset().top - 110
+        // },
+        // 800,
+        // function() {
           // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        }
-      );
-    } // End if
-  });
+          // window.location.hash = hash;
+        // }
+      // );
+    // } // End if
+  // });
 
-  $(".navbar-brand img").click(function() {
-    $("body, html").animate({ scrollTop: 0 }, 1000);
-  });
+  // $(".navbar-brand img").click(function() {
+  //   $("body, html").animate({ scrollTop: 0 }, 1000);
+  // });
 
   // Get the modal
   var modal = document.getElementById("myModal");
 
   // Get the button that opens the modal
   var btn = document.getElementById("start-now");
+  var btn2 = document.getElementById("start-now2");
+  var btn3 = document.getElementById("start-now3");
+  // var btn = document.getElementsByClassName("modal-btn");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks on the button, open the modal
   btn.onclick = function() {
+    modal.style.display = "block";
+  };
+  btn2.onclick = function() {
+    modal.style.display = "block";
+  };
+  btn3.onclick = function() {
     modal.style.display = "block";
   };
 
